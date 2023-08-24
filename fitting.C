88ed2fefe8ca60ec573369dc05d231962235fe12
RY6423 <tauryo.31661144.nitayu@gmail.com>
2023-08-24 12:11:11 +0900

    飯だー

--- fitting.C
+++ fitting.C
@@ -26,6 +26,9 @@ void fitting(int file_n_pre) {
   //ファイルオープン
   std::string file_n = std::to_string(file_n_pre);
   std::string file_ini = "ksc_"+std::string(std::max(0, 3 - (int)file_n.size()), '0') + file_n;
+  std::string file = "out/" + file_ini + "/" + file_ini + "_analize.root";
+  TFile* tf = TFile::Open(file.c_str());
+  TGraphErrors* th1 = (TGraphErrors*)tf->Get("Graph");
 
   //[0]..高さ、[1]..波数、[2]..幅、[3]..中心間隔、[4]..距離、[5]..バックグラウンド、[6]..中心座標
   TF1* func = new TF1("func","4*[0]*TMath::Power(TMath::Sin([1]*[2]*TMath::Sin(TMath::ATan2(x-[6],[4]))/2)/([1]*[2]*TMath::Sin(TMath::ATan2(x-[6],[4]))/2)*TMath::Cos([1]*[3]*TMath::Sin(TMath::ATan2(x-[6],[4]))/2),2)+[5]",0,100);
@@ -33,5 +36,6 @@ void fitting(int file_n_pre) {
   TF1Convolution* f_conv = new TF1Convolution("func", "f_uniform", 0, 100, true);
   TF1* f = new TF1("f", *f_conv, 0, 100, f_conv->GetNpar());
   f->SetParameters(10,1428,0.02,0.04,200,1,50);
-  f->Draw();
+  //f->Draw();
+  tf->Draw();
 }
